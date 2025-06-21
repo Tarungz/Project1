139a4f6102996a62907ca6d6df4ef509b65bda53
Rohit Thapa <134378288+Tarungz@users.noreply.github.com>
2025-06-21 08:35:36 +0545

    Add files via upload

---
+++ html code/script.js
@@ -0,0 +1,11 @@
+const slider = document.getElementById("card-slider");
+const btnLeft = document.querySelector(".slide-btn.left");
+const btnRight = document.querySelector(".slide-btn.right");
+
+btnLeft.addEventListener("click", () => {
+  slider.scrollLeft -= 250;
+});
+
+btnRight.addEventListener("click", () => {
+  slider.scrollLeft += 250;
+});
